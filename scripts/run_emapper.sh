f2a1c785f8dc441a629f1cb9d3df62c05690a67e
caleb-easterly <easte080@umn.edu>
2017-08-01 13:12:22 -0500

    reformatting, remove file comments

--- scripts/run_emapper.sh
+++ scripts/run_emapper.sh
@@ -3,7 +3,13 @@
 path_to_emap=$1
 wd=$(pwd)
 
-python $path_to_emap -i $wd/test/nlim_fragment.fasta --output HMM_nlim --output_dir $wd/out --no_file_comments --override -d thaNOG --data_dir $wd/data
-python $path_to_emap -i $wd/test/nlim_fragment.fasta --output DIA_nlim --output_dir $wd/out --no_file_comments --override -d thaNOG -m diamond --data_dir $wd/data
-
+python $path_to_emap -i $wd/test/nlim_fragment.fasta \
+	--output HMM_nlim --output_dir $wd/out \
+	--override --database thaNOG --data_dir $wd/data \
+	--no_file_comments
+	
+python $path_to_emap -i $wd/test/nlim_fragment.fasta \
+	--output DIA_nlim --output_dir $wd/out \
+	--override -d thaNOG -m diamond --data_dir $wd/data \
+	--no_file_comments
 
