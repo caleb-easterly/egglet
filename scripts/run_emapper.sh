9df8a2fcf5d2df89c670935bc3374d7db1337ca2
caleb-easterly <easte080@umn.edu>
2017-08-02 17:14:37 -0500

    simplify analyses - no annotation

--- scripts/run_emapper.sh
+++ scripts/run_emapper.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+# put path to emapper.py
 path_to_emap=$1
+
+# must be run from inside egglet 
 wd=$(pwd)
 
 python $path_to_emap -i $wd/test/nlim_fragment.fasta \
 	--output HMM_nlim --output_dir $wd/out \
 	--override \
-	--database thaNOG \
-	--no_refine \
-	--data_dir $wd/data	
+	--database $wd/data/ENOG411CB2I/ENOG411CB2I \
+	--data_dir $wd/data\
+	--no_refine\
+	--no_annot	
 		
 python $path_to_emap -i $wd/test/nlim_fragment.fasta \
-	--output DIA_nlim --output_dir $wd/out \
-	--override -m diamond --data_dir $wd/data \
-	--no_file_comments
+	--output DIA_nlim \
+	--output_dir $wd/out \
+	--override \
+	-m diamond \
+	--data_dir $wd/data \
+	--no_file_comments \
+	--no_annot
 
